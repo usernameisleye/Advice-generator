849da0d1cd7be0b64b5f0f6e17215542eeba11b4
Adesida Leye <adeleyeadesida@gmail.com>
2022-12-30 14:19:28 +0100

    update on files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ['"Manrope"', 'sans-serif'],
+    },
+    colors: {
+      'dark-blue': 'hsl(218, 23%, 16%)',
+      'grayish-blue': 'hsl(217, 19%, 38%)',
+      'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+      'white': '#ffffff',
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'neon-green': 'hsl(150, 100%, 66%)',
+    }
   },
   plugins: [],
 }
