e5d99fabe5ecbcfd3f4735d1ef00b2b0a46ba216
Adesida Leye <adeleyeadesida@gmail.com>
2023-01-02 13:11:33 +0100

    update on files

--- public/script.js
+++ public/script.js
@@ -6,10 +6,7 @@ generateAdviceBtn.addEventListener('click', generateAdvice);
 
 function generateAdvice(){
     fetch('https://api.adviceslip.com/advice').then(res => res.json()).then(newAdvice =>{
-        advice.innerHTML = newAdvice.slip.advice;
+        advice.innerHTML = `"${newAdvice.slip.advice}"`;
         adviceNum.innerHTML = newAdvice.slip.id;
     })
 }
-
-0px 0px 18px rgba(255, 255, 255, 0.3)
-0px 0px 18px hsl(150, 100%, 66%)
